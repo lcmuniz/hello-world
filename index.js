108d0018f5881cb77fbdbe0a54377a16c60154b9
= <=>
2020-07-26 00:46:31 -0300

    ok

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 
 const app = express()
 
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({status: 'running'})
 })
 
-app.listen(8080)
-console.log('Server listening on port 8080')
+app.listen(server_port, server_ip_address, function() {
+  console.log('Server started')
+})

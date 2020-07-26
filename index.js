95b2050a3bbdb49afff55ebcb0044c0d87fa6360
= <=>
2020-07-26 00:34:15 -0300

    initial

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.json({status: 'running'})
 })
 
 app.listen(8080)
+console.log('Server listening on port 8080')

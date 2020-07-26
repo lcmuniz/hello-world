e92ceea036a920b7730208bd0c9b5f15a69ff9e0
= <=>
2020-07-26 00:55:42 -0300

    ok1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({status: 'running'})
+app.get('/', function (req, res) {
+  res.json({status: 'running'});
 })
 
 app.listen(server_port, server_ip_address, function() {
-  console.log('Server started')
+  console.log('Server started');
 })
 
-module.exports = app
+module.exports = app;
